d264004e9a38bb6954d80ff8fe9b892f9c52e246
Watts-Blake <watts.blake52@gmail.com>
2022-04-12 10:40:27 -0700

    fixed hashed password problem

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,27 +5,27 @@ from app.models import db, User
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
-    akatsuki = [{'username': 'Itachi', 'email': 'itachi@demo.com', 'hashed_password': 'itachi', 'profile_picture': 'http://pm1.narvii.com/6689/fd31532cd6c492fe3f3d3e3e449336672a15c5ff_00.jpg'},{'username': 'Madara', 'email': 'madara@demo.com', 'hashed_password': 'itachi', 'profile_picture': 'https://www.pngitem.com/pimgs/m/122-1222641_beautiful-naruto-png-madara-funny-face-madara-uchiha.png'},{'username': 'Obito', 'email': 'obito@demo.com', 'hashed_password': 'itachi', 'profile_picture': 'https://i.pinimg.com/550x/2d/72/4a/2d724ac3c6db73eb619b4c6c957cdb60.jpg'},{'username': 'Nagato', 'email': 'nagato@demo.com', 'hashed_password': 'itachi', 'profile_picture': 'https://i.pinimg.com/736x/1a/9f/fb/1a9ffbfd168de648eef4c033061d7393--game-art-naruto.jpg'},{'username': 'Orochimaru', 'email': 'oruchimaru@demo.com', 'hashed_password': 'itachi', 'profile_picture': 'https://i.pinimg.com/236x/9b/06/68/9b06680c73cf145d20f9742bb7961779--naruto-pics-anime-naruto.jpg'}]
+    akatsuki = [{'username': 'Itachi', 'email': 'itachi@demo.com', 'password': 'itachi', 'profile_picture': 'http://pm1.narvii.com/6689/fd31532cd6c492fe3f3d3e3e449336672a15c5ff_00.jpg'},{'username': 'Madara', 'email': 'madara@demo.com', 'password': 'itachi', 'profile_picture': 'https://www.pngitem.com/pimgs/m/122-1222641_beautiful-naruto-png-madara-funny-face-madara-uchiha.png'},{'username': 'Obito', 'email': 'obito@demo.com', 'password': 'itachi', 'profile_picture': 'https://i.pinimg.com/550x/2d/72/4a/2d724ac3c6db73eb619b4c6c957cdb60.jpg'},{'username': 'Nagato', 'email': 'nagato@demo.com', 'password': 'itachi', 'profile_picture': 'https://i.pinimg.com/736x/1a/9f/fb/1a9ffbfd168de648eef4c033061d7393--game-art-naruto.jpg'},{'username': 'Orochimaru', 'email': 'oruchimaru@demo.com', 'password': 'itachi', 'profile_picture': 'https://i.pinimg.com/236x/9b/06/68/9b06680c73cf145d20f9742bb7961779--naruto-pics-anime-naruto.jpg'}]
 
     for user in akatsuki:
-        new_user = User(username=user['username'], email=user['email'], hashed_password=user['hashed_password'], profile_picture=user['profile_picture'])
+        new_user = User(username=user['username'], email=user['email'], password=user['password'], profile_picture=user['profile_picture'])
         db.session.add(new_user)
 
-    scouts = [ {'username': 'Eren', 'email': 'eren@demo.com', 'hashed_password': 'eren', 'profile_picture': 'https://cdn.vox-cdn.com/thumbor/rOYixubHAmop1DUJXdoEtzCuypY=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22474122/Screenshotter_HuluWatch_2050.0.jpeg'},{'username': 'Levi', 'email': 'levi@demo.com', 'hashed_password': 'eren', 'profile_picture': 'https://i.pinimg.com/originals/d4/b3/3e/d4b33ef38b4dd76ac2acc1745327bf99.jpg'},{'username': 'Mikasa', 'email': 'mikasa@demo.com', 'hashed_password': 'eren', 'profile_picture': 'https://attackontitanstuff.com/wp-content/uploads/2021/06/new-attack-on-titan-mikasa-car-stickers-beauty-fine-decals-243-1.jpg'}, {'username': 'Armin', 'email': 'armin@demo.com', 'hashed_password': 'eren', 'profile_picture': 'https://i.pinimg.com/originals/1f/fa/8e/1ffa8e0a04d88e0dc0655340f606664c.png'},{'username': 'Erwin', 'email': 'erwin@demo.com', 'hashed_password': 'eren', 'profile_picture': 'https://i.pinimg.com/736x/5b/35/7d/5b357d14e5a6bcdfe1318accc19ef265.jpg'}, ]
+    scouts = [ {'username': 'Eren', 'email': 'eren@demo.com', 'password': 'eren', 'profile_picture': 'https://cdn.vox-cdn.com/thumbor/rOYixubHAmop1DUJXdoEtzCuypY=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22474122/Screenshotter_HuluWatch_2050.0.jpeg'},{'username': 'Levi', 'email': 'levi@demo.com', 'password': 'eren', 'profile_picture': 'https://i.pinimg.com/originals/d4/b3/3e/d4b33ef38b4dd76ac2acc1745327bf99.jpg'},{'username': 'Mikasa', 'email': 'mikasa@demo.com', 'password': 'eren', 'profile_picture': 'https://attackontitanstuff.com/wp-content/uploads/2021/06/new-attack-on-titan-mikasa-car-stickers-beauty-fine-decals-243-1.jpg'}, {'username': 'Armin', 'email': 'armin@demo.com', 'password': 'eren', 'profile_picture': 'https://i.pinimg.com/originals/1f/fa/8e/1ffa8e0a04d88e0dc0655340f606664c.png'},{'username': 'Erwin', 'email': 'erwin@demo.com', 'password': 'eren', 'profile_picture': 'https://i.pinimg.com/736x/5b/35/7d/5b357d14e5a6bcdfe1318accc19ef265.jpg'}, ]
 
     for user in scouts:
-        new_user = User(username=user['username'], email=user['email'], hashed_password=user['hashed_password'], profile_picture=user['profile_picture'])
+        new_user = User(username=user['username'], email=user['email'], password=user['password'], profile_picture=user['profile_picture'])
         db.session.add(new_user)
 
-    aa = [ {'username': 'Blake', 'email': 'blake@demo.com', 'hashed_password': 'blake', 'profile_picture': ''},{'username': 'Yake', 'email': 'yake@demo.com', 'hashed_password': 'blake', 'profile_picture': ''},{'username': 'Ali', 'email': 'ali@demo.com', 'hashed_password': 'blake', 'profile_picture': ''}, {'username': 'Will', 'email': 'will@demo.com', 'hashed_password': 'blake', 'profile_picture': ''},{'username': 'Nick', 'email': 'nick@demo.com', 'hashed_password': 'blake', 'profile_picture': ''},{'username': 'ANT', 'email': 'ant@demo.com', 'hashed_password': 'blake', 'profile_picture': ''},{'username': 'Braxton', 'email': 'brax@demo.com', 'hashed_password': 'blake', 'profile_picture': ''}, ]
+    aa = [ {'username': 'Blake', 'email': 'blake@demo.com', 'password': 'blake', 'profile_picture': ''},{'username': 'Yake', 'email': 'yake@demo.com', 'password': 'blake', 'profile_picture': ''},{'username': 'Ali', 'email': 'ali@demo.com', 'password': 'blake', 'profile_picture': ''}, {'username': 'Will', 'email': 'will@demo.com', 'password': 'blake', 'profile_picture': ''},{'username': 'Nick', 'email': 'nick@demo.com', 'password': 'blake', 'profile_picture': ''},{'username': 'ANT', 'email': 'ant@demo.com', 'password': 'blake', 'profile_picture': ''},{'username': 'Braxton', 'email': 'brax@demo.com', 'password': 'blake', 'profile_picture': ''}, ]
 
     for user in aa:
-        new_user = User(username=user['username'], email=user['email'], hashed_password=user['hashed_password'], profile_picture=user['profile_picture'])
+        new_user = User(username=user['username'], email=user['email'], password=user['password'], profile_picture=user['profile_picture'])
         db.session.add(new_user)
 
 
     demo = User(
-        username='Demo', email='demo@demo.com', hashed_password='password', profile_picture='https://logos-world.net/wp-content/uploads/2020/12/Discord-Logo.png')
+        username='Demo', email='demo@demo.com', password='password', profile_picture='https://logos-world.net/wp-content/uploads/2020/12/Discord-Logo.png')
 
     db.session.add(demo)
 
