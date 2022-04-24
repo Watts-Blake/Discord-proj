40b9144be1a4c337aa1e6115f68ab0cf56c65dd5
Watts-Blake <watts.blake52@gmail.com>
2022-04-24 06:31:57 -0700

    fixed channel editing wrong channel bug

--- react-app/src/components/EditChannel/EditChannelModal.js
+++ react-app/src/components/EditChannel/EditChannelModal.js
@@ -1,17 +1,19 @@
 import "./Modal.css";
 
+import { getOneChannel } from "../../store/channels";
 import { Modal } from "../../context/Modal";
+import { useDispatch } from "react-redux";
 import EditChannel from ".";
 import { useState } from "react";
 const EditChannelModal = ({ channel, user }) => {
   const [showModal, setShowModal] = useState(false);
+  const dispatch = useDispatch();
+  const handleOpen = async () => {
+    await dispatch(getOneChannel(channel.id)).then(() => setShowModal(true));
+  };
   return (
     <>
-      <img
-        src="/svgs/settings.svg"
-        alt="add"
-        onClick={() => setShowModal(true)}
-      />
+      <img src="/svgs/settings.svg" alt="add" onClick={handleOpen} />
       {showModal && (
         <Modal className="modal" onClose={() => setShowModal(false)}>
           <EditChannel
