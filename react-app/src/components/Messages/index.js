5acc7dba619fbd29096392ed994b5280eee93bc9
Watts-Blake <watts.blake52@gmail.com>
2022-06-01 16:44:14 -0700

    hover effect class add complete

--- react-app/src/components/Messages/index.js
+++ react-app/src/components/Messages/index.js
@@ -1,8 +1,10 @@
 import "./Messages.css";
 
 import { useRef, useEffect } from "react";
+import { useState } from "react";
 
 const Messages = ({ messages }) => {
+  const [hover, setHover] = useState(false);
   let messagesEnd = useRef(null);
   useEffect(() => {
     let isActive = true;
@@ -11,10 +13,16 @@ const Messages = ({ messages }) => {
       isActive = false;
     };
   }, [messages]);
+
   return (
     <div className="messages">
       {messages?.map((message) => (
-        <div className="message" key={message.id}>
+        <div
+          className={hover === message.id ? "curr_message" : "message"}
+          key={message.id}
+          onMouseOver={() => setHover(message.id)}
+          onMouseLeave={() => setHover(false)}
+        >
           <div className="message_user">
             <img
               className="message_pfp"
@@ -26,6 +34,9 @@ const Messages = ({ messages }) => {
             <h4 className="username">{message.senderUsername}</h4>
             <p>{message.content}</p>
           </div>
+          {hover === message.id && (
+            <img src="/svgs/dot-dot.svg" alt="more" className="delete"></img>
+          )}
         </div>
       ))}
       <div ref={messagesEnd}></div>
