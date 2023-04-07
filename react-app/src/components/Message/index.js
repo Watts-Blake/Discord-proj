3f6e41d54cf70ce904dc935e9a3e4ffcc013c9c5
Watts-Blake <watts.blake52@gmail.com>
2023-04-06 21:17:37 -0700

    styled edited messages, fixed socket connection bug by moving socket io invocation to the socket useEffect()

--- react-app/src/components/Message/index.js
+++ react-app/src/components/Message/index.js
@@ -8,6 +8,24 @@ const Message = ({ message, handleDeleteMessage, handleUpdateMessage }) => {
   const [hover, setHover] = useState(false);
   const [options, setOptions] = useState(null);
   const [showEditMessage, setShowEditMessage] = useState(false);
+  const checkIfToday = (date) => {
+    const today = new Date().getDay();
+    const dateToCheck = new Date(date).getDay();
+    if (today === dateToCheck) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  const checkIfYesterday = (date) => {
+    const yesterday = new Date().getDay() - 1;
+    const dateToCheck = new Date(date).getDay();
+    if (yesterday === dateToCheck) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   const user = useSelector((state) => state.session.user);
   const server = useSelector((state) => state.servers.currentServer);
@@ -35,7 +53,26 @@ const Message = ({ message, handleDeleteMessage, handleUpdateMessage }) => {
 
       <div className="message_content">
         <div className="message_header">
-          <h4 className="username">{message.senderUsername}</h4>
+          <div className="message_header_left">
+            <p className="username">{message.senderUsername}</p>
+            <div className="message_time">
+              <p>
+                {checkIfToday(message.createdAt)
+                  ? "Today"
+                  : checkIfYesterday(message.createdAt)
+                  ? "Yesterday"
+                  : new Date(message.createdAt).toLocaleDateString()}
+              </p>
+              <p>at</p>
+              <p>
+                {new Date(message.createdAt).toLocaleTimeString("locales", {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </p>
+            </div>
+          </div>
+
           {hover === message.id && message.senderId !== 1 && (
             <div className="message_more">
               <button
@@ -50,7 +87,14 @@ const Message = ({ message, handleDeleteMessage, handleUpdateMessage }) => {
             </div>
           )}
         </div>
-        {showEditMessage !== message.id && <span>{message.content}</span>}
+        {showEditMessage !== message.id && (
+          <div className="message_content_message_container">
+            <span className="message_content_message">{message.content}</span>
+            {message.updatedAt && (
+              <p className="message_content_edited_message">(edited)</p>
+            )}
+          </div>
+        )}
         {showEditMessage === +message.id && (
           <ChatInput
             messageToEdit={message}
