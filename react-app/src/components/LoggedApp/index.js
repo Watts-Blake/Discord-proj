842ded477f2aa129da867c60647bd096f3aa1d65
Watts-Blake <watts.blake52@gmail.com>
2023-04-11 23:15:22 -0700

    took out unused vars and uneeded commnts from logged app

--- react-app/src/components/LoggedApp/index.js
+++ react-app/src/components/LoggedApp/index.js
@@ -2,53 +2,41 @@ import LeftNavBar from "../LeftNavBar";
 import ProtectedRoute from "../auth/ProtectedRoute";
 import MainContent from "../MainContent";
 import AllServers from "../AllServers";
-// import { logout } from "../../store/session";
+
 import {
   setActiveUsers,
   activateUser,
   deactivateUser,
   setUserIdle,
 } from "../../store/users";
+
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
-// import { useHistory } from "react-router-dom";
+
 import { io } from "socket.io-client";
 let socket;
 
 const LoggedApp = ({ user, loggedAppLoaded, setLoggedAppLoaded }) => {
-  // const user = useSelector((state) => state.session.user);
-  // const history = useHistory();
   const dispatch = useDispatch();
   const servers = useSelector((state) => state.servers);
   const userServers = Object.values(servers.userServers);
   const onlineUsers = useSelector((state) => state.users);
-  // const [currUserActive, setCurrUserActive] = useState(false);
   const [deactiveTimeOutId, setDeactiveTimeOutId] = useState();
   const [idleTimeOutId, setIdleTimeOutId] = useState();
-  // const [socket, setSocket] = useState(io());
 
   useEffect(() => {
     socket = io();
     socket.on("connect", (data) => {
-      console.log("right here ------->", data);
       if (data) {
         dispatch(activateUser(data));
         setLoggedAppLoaded(true);
       }
-
-      // if (data.activatedUser.user.id === user.id) {
-      //   setCurrUserActive(true);
-      //   setLoggedAppLoaded(true);
-      // }
     });
     socket.on("retrieve_active_users", (data) => {
       dispatch(setActiveUsers(data));
     });
     socket.on("set_idle_user", (data) => {
       dispatch(setUserIdle(data));
-      // if (data.activatedUser.user.id === user.id) {
-      //   setCurrUserActive(false);
-      // }
     });
     socket.on("deactivate_user", async (data) => {
       dispatch(deactivateUser(data));
@@ -67,11 +55,11 @@ const LoggedApp = ({ user, loggedAppLoaded, setLoggedAppLoaded }) => {
   }, []);
 
   useEffect(() => {
-    console.log("im runnning");
     socket.emit("join_room", { room: `user: ${user.id}` });
     socket.emit("retrieve_active_users", { room: `user: ${user.id}` });
     //eslint-disable-next-line
   }, []);
+
   const handleUserActivity = () => {
     clearTimeout(idleTimeOutId);
     clearTimeout(deactiveTimeOutId);
@@ -102,7 +90,6 @@ const LoggedApp = ({ user, loggedAppLoaded, setLoggedAppLoaded }) => {
     loggedAppLoaded && (
       <div className="logged_app" onMouseMove={handleUserActivity}>
         <LeftNavBar userServers={userServers} user={user} />
-
         <ProtectedRoute path="/channels">
           <MainContent className="main_content" user={user} />
         </ProtectedRoute>
